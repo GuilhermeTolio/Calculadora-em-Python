e001156926615466801b2acf6234436b6486a2e4
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2022-05-17 18:51:31 -0300

    calcular novamente

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-#solicitar qual operacao deseja ser feita
-operacao = input('''
-Digite a operação matemática que deseja concluir:
-+ para adição 
-- para subtração 
-* para multiplicação 
-/ para divisão
-''')
+#definindo funcoes
 
+def calculadora():
+    #solicitar qual operacao deseja ser feita
+    operacao = input('''
+    Digite a operação matemática que deseja concluir:
+    + para adição 
+    - para subtração 
+    * para multiplicação 
+    / para divisão
+    ''')
 
-#solicitar o numero para o usuario
-numero_1 = int(input('Coloque seu primeiro numero: '))
-numero_2 = int(input('Coloque seu segundo numero: '))
+    #solicitar o numero para o usuario
+    numero_1 = int(input('Coloque seu primeiro numero: '))
+    numero_2 = int(input('Coloque seu segundo numero: '))
 
-#codigo da operacao
-if operacao == '+':
-    print('{} + {} = {}'.format(numero_1, numero_2, numero_1 + numero_2))
-elif operacao == '-':
-    print('{} - {} = {}'.format(numero_1, numero_2, numero_1 - numero_2))
-elif operacao == '*':
-    print('{} * {} = {}'.format(numero_1, numero_2, numero_1 * numero_2))
-elif operacao == '/':
-    print('{} / {} = {}'.format(numero_1, numero_2, numero_1 / numero_2))
+    #codigo da operacao
+    if operacao == '+':
+        print('{} + {} = {}'.format(numero_1, numero_2, numero_1 + numero_2))
+    elif operacao == '-':
+        print('{} - {} = {}'.format(numero_1, numero_2, numero_1 - numero_2))
+    elif operacao == '*':
+        print('{} * {} = {}'.format(numero_1, numero_2, numero_1 * numero_2))
+    elif operacao == '/':
+        print('{} / {} = {}'.format(numero_1, numero_2, numero_1 / numero_2))
 
-# operador nao valido
-else:
-    print('O operador escolhido nao eh valido, tente novamente')
+    # operador nao valido
+    else:
+        print('O operador escolhido nao eh valido, tente novamente')
+
+#calcular de novo
+def novamente():
+    calcular_novamente = input('''
+    Deseja calcular novamente?
+    Y para sim e N para nao
+    ''')
+    if
+calculadora()
