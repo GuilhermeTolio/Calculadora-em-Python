79c4efdf53ef409d5f5457986022ee46d1aaa65c
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2022-05-17 18:43:11 -0300

    solicitacao da operacao e operador nao valido

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+#solicitar qual operacao deseja ser feita
+operacao = input('''
+Digite a operação matemática que deseja concluir:
++ para adição 
+- para subtração 
+* para multiplicação 
+/ para divisão
+''')
+
+
 #solicitar o numero para o usuario
 numero_1 = int(input('Coloque seu primeiro numero: '))
 numero_2 = int(input('Coloque seu segundo numero: '))
 
 #adicionando operadores
 #adicao
-print(numero_1 + numero_2)
+if operacao == '+':
+    print(numero_1 + numero_2)
+elif operacao == '-':
+    print(numero_1 - numero_2)
+elif operacao == '*':
+    print(numero_1 * numero_2)
+elif operacao == '/':
+    print(numero_1 / numero_2)
 
-#subtracao
-print(numero_1 - numero_2)
-
-#multiplicacao
-print(numero_1 * numero_2)
-
-#Divisao
-print(numero_1 / numero_2)
+# operador nao valido
+else:
+    print('O operador escolhido nao eh valido, tente novamente')
