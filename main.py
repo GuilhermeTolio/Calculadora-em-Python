3ad07cdea2eacd235ac270b9a491f5651ebf53f2
Guilherme Tolio dos Santos <guilhermetolio@gmail.com>
2022-05-17 18:31:33 -0300

    #adicionando operadores

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ numero_1 = int(input('Coloque seu primeiro numero: '))
 numero_2 = int(input('Coloque seu segundo numero: '))
 
 #adicionando operadores
+#adicao
+print(numero_1 + numero_2)
 
+#subtracao
+print(numero_1 - numero_2)
+
+#multiplicacao
+print(numero_1 * numero_2)
+
+#Divisao
+print(numero_1 / numero_2)
